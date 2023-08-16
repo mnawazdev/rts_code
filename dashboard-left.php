48361543d7406fa8874e7d6ccd4cb97cd3053899
Muqeem Nawaz <125767113+mnawazdev@users.noreply.github.com>
2023-08-16 18:36:32 +0500

    Create dashboard-left.php

---
+++ dashboard-left.php
@@ -0,0 +1,7 @@
+/* Added contact form and phone number to member site */
+
+/* Added support link to the left of the members area dashboard and upon clicking, it goes to the support page support.php */ 
+
+<?php 
+  <li><a href="<?php echo $home_url; ?>?support"><span><img src="<?php echo get_template_directory_uri(); ?>/images/bsp_new_icon4.png" alt="" /></span>Support</a></li>
+?>
