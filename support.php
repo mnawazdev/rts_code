47e3779291231c6ab408b8a95f3260295f359f70
Muqeem Nawaz <125767113+mnawazdev@users.noreply.github.com>
2023-08-16 18:40:05 +0500

    Update support.php

--- support.php
+++ support.php
@@ -1,1 +1,15 @@
+/* Support Page Code */
 
+<?php 
+/* Template Name: Support */
+
+?>
+        <div class="mid_white">
+            <div class="midarea">
+                <div class="shortarea">
+                    <label>Support</label>
+                    <p>Need assistance? Please call us at or fill out the form below.</p>
+                </div>
+                <?php echo do_shortcode('[contact-form-7 id="25828" title="Support Contact Form"]'); ?>
+            </div>
+        </div>
